828d6f400eae5a3454ed2ed25bf7c4c143f3277c
tomasgab <gbena008@fiu.edu>
2020-01-26 15:15:21 -0500

    -Fixed placement of divs, added link to email, dynamic resizing with JS.

--- index.js
+++ index.js
@@ -1,47 +1,72 @@
 
 
- var aboutMeContent = "Hi, i'm Gabriel and I am a Software developer, \
-I hold a Bachelor's of Science degree in Information Technology from Florida International University\
-with a concentration in software development. I am a self-motivated software developer with\
+var aboutMeHeader = 'About Me'; 
+ 
+var aboutMeContent = "Hi, i'm Gabriel and I am a Software developer living in Miami, Fl. \
+I hold a Bachelor's of Science degree in Information Technology from Florida International University \
+with a concentration in software development. I am a self-motivated software developer with \
 a year plus of experience in Java development and server-side administration. \
 I am an incisive thinker and solution-oriented team member. \
 I have experience in different areas of development such as Java programming and other back-end frameworks/languages, \
 database administration, Operating Systems, and front-end development. I enjoy connecting with others to share ideas and collaborate. ";
 
+
+var contactHeader= 'Contact';
+
 var contactInfo = '\
-            Email: gbena008@fiu.edu <br>\
-            Phone: (786) 985-9390 <br>\
+            Email: <a href="mailto:gbena008@fiu.edu">gbena008@fiu.edu</a> <br><br>\
+            Phone: (786) 985-9390 <br><br>\
             Location: Miami, FL';
 
-var contactHeader = 'Contact';
-var aboutMeHeader = 'About Me';
 
 
 var home=document.getElementsByClassName("home")[0];
 var contact=document.getElementsByTagName("contact")[0];
-var td=document.getElementsByTagName("td")[0];
-var headerTag = document.getElementsByClassName("contactInfoHeader");
-var aboutMeHeaderTag = document.getElementsByClassName("aboutMeHeader");
+var contentHeader = document.getElementsByClassName("contentHeader")[0];
+var content = document.getElementsByClassName("content")[0];
 
 
 function initialize(){
-    changeContent(aboutMeHeaderTag, aboutMeHeHeader);
-    changeContent(td, aboutMeContent);
+    homeClickAction();
+      /** @type {HTMLElement} */
+      var contentElement = document.querySelector(".content");
+      contentElement.style.left = "120px";
+      contentElement.style.width = "500px";
+      contentElement.style.height = "200px";
+      contentElement.style.top = "45px";
 }
 
 function homeClickAction(){ 
-    if(!td.innerHTML.match.aboutMeContent){
-        changeContent(aboutMeHeaderTag, aboutMeHeader);
-        changeContent(td, aboutMeContent);
+    if(!content.innerHTML.match.aboutMeContent){
+        changePageContent(contentHeader, aboutMeHeader);
+        changePageContent(content, aboutMeContent);
+      /** @type {HTMLElement} */
+      var contentElement = document.querySelector(".content");
+      contentElement.style.left = "120px";
+      contentElement.style.width = "500px";
+      contentElement.style.height = "200px";
+      contentElement.style.top = "45px";
     }
 }
 
 function contactClickAction(){ 
-    changeContent(contactHeaderTag, contactHeader);
-    changeContent(td, contactInfo);
+    changePageContent(contentHeader, contactHeader);
+    changePageContent(content, contactInfo);
+    /** @type {HTMLElement} */
+    var contentElement = document.querySelector(".content");
+    contentElement.style.left = "285px";
+    contentElement.style.width = "300px";
+    contentElement.style.height = "200px";
+    contentElement.style.top = "45px";
 }
 
-function changeContent(element, content){
+function changePageContent(element, content){
     element.innerHTML = content;
 }
+
+
+
+
+
+
 
